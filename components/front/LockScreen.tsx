4da034cb775f34ad95a8817b1d100ca23a566c37
iroid <iradtu2@gmail.com>
2024-12-19 18:42:37 +0300

    xom3

--- components/front/LockScreen.tsx
+++ components/front/LockScreen.tsx
@@ -12,7 +12,7 @@ export default function LockScreen({ onUnlock }: { onUnlock: () => void }) {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <Image
-        src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+        src="/images/lock.webp"
         alt="Lock screen background"
         layout="fill"
         objectFit="cover"
